34555332209da67482ad3c8e4a2eba72353cb0ad
Sharon <jsmoon883@gmail.com>
2026-05-23 21:52:59 -0500

    separate docs & add loot

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,9 @@
 import { useState, useEffect } from 'react'
 import { idleFrames, grabFrames, lakeFrames } from "./animations/me_animations";
-import blue from "./assets/blobs/blue.png"
-import green from "./assets/blobs/green.png"
-import yellow from "./assets/blobs/yellow.png"
-import red from "./assets/blobs/red.png"
-import Carrot from "./assets/bait/carrot.svg?react";
-import Lure from "./assets/bait/lure.svg?react";
-import Shrimp from "./assets/bait/shrimp.svg?react";
-import Worm from "./assets/bait/worm.svg?react";
 
-import Scroll from "./assets/suick.svg?react";
+import Hero from "./Hero";
+import Popup from "./Popup";
 
-function Hero({ onBaitClick, lakeImage, meImage }){
-    return(
-        <div className="hero-container"> 
-            <div className="intro-text">
-                <h2>Hi I'm Sharon !</h2>
-                <p>Welcome to my website. I recently graduated from <strong>Rice University</strong> with a <strong>B.A. in Computer Science</strong> and spent two summers interning at <span className="red-text"><strong>Pinterest</strong></span>. I’m passionate about combining creativity and engineering to build elegant solutions with real world impact.</p>
-            </div>
-            <img className="lake" src={lakeImage} alt="lake"/>
-            <img className="me" src={meImage} alt="me"/>
-            <div className = "bait-container">
-                <div className="bait-section">
-                    <Carrot className="bait flipped" onClick={() => onBaitClick("carrot")}/>
-                    <div className="blob-container">
-                        <img className="blob" src={green} alt="green"/>
-
-                        <div className="blob-text">
-                            <h4>about me</h4>
-                            <p>hello hwllo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="bait-section">
-                    <Lure className="bait" onClick={() => onBaitClick("lure")}/>  
-                    <div className="blob-container">
-                        <img className="blob" src={red} alt="red"/>
-
-                        <div className="blob-text">
-                            <h4>projects</h4>
-                            <p>hello hwllo</p>
-                        </div>
-                    </div>                
-                </div>
-                <div className="bait-section">
-                    <Shrimp className="bait" onClick={() => onBaitClick("shrimp")}/>  
-                    <div className="blob-container">
-                        <img className="blob" src={blue} alt="blue"/>
-
-                        <div className="blob-text">
-                            <h4>experience</h4>
-                            <p>hello hwllo</p>
-                        </div>
-                    </div>                
-                </div>
-                <div className="bait-section">
-                    <Worm className="bait" onClick={() => onBaitClick("worm")}/> 
-                    <div className="blob-container">
-                        <img className="blob" src={yellow} alt="yellow"/>
-
-                        <div className="blob-text">
-                            <h4>contact</h4>
-                            <p>hello hwllo</p>
-                        </div>
-                    </div>                
-                </div>  
-            </div>
-        </div>
-    );
-}
-
-function Popup({ value, onClose }) {
-    return (
-      <div className="popup-background" onClick={onClose}>
-        <Scroll
-            className="popup-img"
-            onClick={(e) => {
-            e.stopPropagation();
-            console.log("clicked painted region");
-            }}
-        />
-        <div className="popup" onClick={(e) => e.stopPropagation()}>
-          <h1>{value}</h1>
-          <button onClick={onClose}>close</button>
-        </div>
-      </div>
-    );
-}
 
 function Header(){
     return (
---
+++ src/Popup.jsx
@@ -0,0 +1,129 @@
+import { FaBook } from "react-icons/fa";
+import { IoMusicalNotes } from "react-icons/io5";
+import { IoCodeSlash } from "react-icons/io5";
+import { FaPeopleGroup } from "react-icons/fa6";
+
+import "./Popup.css";
+import octopus from "./assets/loot/oct.png"
+import trout from "./assets/loot/trout.png"
+import rabbit from "./assets/loot/rab.png"
+import crab from "./assets/loot/crab.png"
+
+
+function RabbitPopup({ onClose }){
+    return (
+        <div className="popup-background" onClick={onClose}>
+            <img className="popup-img" src={rabbit} alt="rabbit"/>
+            <div className="rabbit-text" onClick={(e) => e.stopPropagation()}>
+                <button className="x-button" onClick={onClose}>x</button>
+                <h1>About Me</h1>
+                <p>hi i am sharon ! outside of coding i love music & art. In my free time, you'll find me playing guitar, drawing, cooking, or reading.</p>
+                <p>~</p>
+                <p><FaBook /> Current read: Crime and Punishment</p>
+                <p><IoMusicalNotes /> Current listenings: Steely Dan </p>
+                
+            </div>
+        </div>
+      );
+}
+
+function TroutPopup({ onClose }){
+    return (
+        <div className="popup-background" onClick={onClose}>
+            <img className="popup-img" src={trout} alt="trout"/>
+            <div className="trout-text" onClick={(e) => e.stopPropagation()}>
+                <button className="x-button" onClick={onClose}>x</button>
+                <h1>Personal Projects</h1>
+                <p>
+                    i enjoy making creative projects that mix code, art, and interactivity.
+                    right now my github is still growing so please check it out! <IoCodeSlash />
+                </p>
+            </div>
+        </div>
+      );
+}
+
+function CrabPopup({ onClose }){
+    return (
+        <div className="popup-background" onClick={onClose}>
+            <img className="popup-img" src={crab} alt="crab"/>
+            <div className="crab-text" onClick={(e) => e.stopPropagation()}>
+                <button className="x-button" onClick={onClose}>x</button>
+                <h1>Internship Experience</h1>
+                <p> at pinterest, i got to work on a real-world codebase with
+                    other amazing engineers. i had the opportunity to work in ads and monetization the first summer
+                    and iOS platform in the second, where i got to see both the product facing and internal sides. <FaPeopleGroup />
+                </p>
+
+                <p>scroll down for more info about my projects !</p>
+            </div>
+        </div>
+      );
+}
+
+function sendEmail(e) {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const message = e.target.message.value;
+
+    const subject = encodeURIComponent(`Message from ${name}`);
+
+    const body = encodeURIComponent(message);
+
+    window.location.href =
+        `mailto:youremail@gmail.com?subject=${subject}&body=${body}`;
+}
+
+function OctPopup({ onClose }){
+    return (
+        <div className="popup-background" onClick={onClose}>
+            <img className="popup-img oct-img" src={octopus} alt="oct"/>
+            <div className="oct-text" onClick={(e) => e.stopPropagation()}>
+                <button className="x-button" onClick={onClose}>x</button>
+                <h1>Contact Me</h1>
+                <form
+                    onSubmit={sendEmail}
+                    className="contact-form"
+                >
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your name"
+                        required
+                    />
+
+                    <textarea
+                        name="message"
+                        placeholder="Your message"
+                        rows="5"
+                        required
+                    />
+
+                    <button type="submit" className="submit-btn">
+                        Send!
+                    </button>
+                </form>
+            </div>
+        </div>
+      );
+}
+
+
+function Popup({ value, onClose }) {
+    if (value == "carrot") {
+        return (<RabbitPopup onClose={onClose}/>)
+    }
+    else if (value == "lure"){
+        return (<TroutPopup onClose={onClose}/>)
+    }
+    else if (value == "shrimp"){
+        return (<CrabPopup onClose={onClose}/>)
+    }
+    else if (value == "worm"){
+        return (<OctPopup onClose={onClose}/>)
+    }
+
+}
+
+export default Popup;
